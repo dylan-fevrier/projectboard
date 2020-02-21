6e9f3681a71d5de3516d7c46cf05829fa01eff3d
Dylan Février <dev@dylanfevrier.com>
2020-02-21 12:48:23 +0100

    Design <Project> : Mise en page de la page index

--- resources/views/projects/index.blade.php
+++ resources/views/projects/index.blade.php
@@ -1,21 +1,27 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="flex items-center">
-        <h1 class="mr-auto">Projects</h1>
-        <a href="/projects/create">New project</a>
-    </div>
+    <header class="flex items-center mb-4 py-4">
+        <div class="flex justify-between w-full items-center">
+            <h2 class="text-grey text-sm mr-auto uppercase">Projects</h2>
+            <a href="/projects/create" class="button button-blue">New project</a>
+        </div>
+    </header>
 
-    <div class="flex">
+    <main class="lg:flex lg:flex-wrap -mx-2">
         @forelse($projects as $project)
-            <div class="bg-white mr-4 rounded shadow w-1/3 p-6" style="height: 200px;">
-                <h3 class="font-normal text-xl py-4"> {{ $project->title }}</h3>
-                <div class="text-grey">
-                    {{ \Illuminate\Support\Str::limit($project->description, 200) }}
+            <div class="lg:w-1/3 px-2 pb-4">
+                <div class="bg-white rounded-md shadow p-6" style="height: 200px;">
+                    <h3 class="font-normal text-xl py-4 -ml-6 border-l-4 border-blue-light pl-5 mb-2">
+                        <a href="{{ $project->path() }}">{{ $project->title }}</a>
+                    </h3>
+                    <div class="text-grey">
+                        {{ \Illuminate\Support\Str::limit($project->description, 200) }}
+                    </div>
                 </div>
             </div>
         @empty
             <li>No project yet.</li>
         @endforelse
-    </div>
+    </main>
 @endsection
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,8 @@ module.exports = {
         colors: {
             "grey-light": "#F5F6F9",
             "grey": "rgba(0,0,0,0.4)",
+            "blue": "#47cdff",
+            "blue-light": "#8ae2fe"
         },
     },
   },
