8ece96d0f7f815f544913f54a7aa0bbb42aebb37
Dylan Février <dev@dylanfevrier.com>
2020-03-09 14:25:56 +0100

    Modification de la page de redirection après login

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -24,6 +24,13 @@ class RouteServiceProvider extends ServiceProvider
     public const HOME = '/home';
 
     /**
+     * The path to the "project" route for this application.
+     *
+     * @var string
+     */
+    public const PROJECT = '/projects';
+
+    /**
      * Define your route model bindings, pattern filters, etc.
      *
      * @return void
