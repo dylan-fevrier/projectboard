f91d6174cd58d74b7604bde081dd25d2b35c70b1
Dylan Février <dev@dylanfevrier.com>
2020-04-05 15:06:22 +0200

    Fix <HomeController> : Bug redirection to homeController

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -21,7 +21,7 @@ class RouteServiceProvider extends ServiceProvider
      *
      * @var string
      */
-    public const HOME = '/home';
+    public const HOME = '/projects';
 
     /**
      * The path to the "project" route for this application.
